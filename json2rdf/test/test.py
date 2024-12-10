6844528bc76c163f36a822947189413d7c433e08
Majid alDosari <majid.aldosari@pnnl.gov>
2024-12-10 17:24:50 -0500

    [json2rdf] testing

---
+++ json2rdf/test/test.py
@@ -0,0 +1,53 @@
+
+
+def json():
+    return {
+        'k': [
+            {'k': 3, 'kk': 'dil', 'kkk':True}
+        ],
+        'kk': { 'k': {'k': 'did'} },
+        'ed': {}, 'el': [],
+        'dwid': {
+            'id': 33,
+            'k': 'v'
+        }
+    }
+
+def json():
+    return {'k':'v', 'id':'id', 'l': [1,2,3] }
+
+
+def rdf():
+    return """
+    prefix rdf:     <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+
+    _:2142013828288 _:id 2142013828288.
+    _:2142013828288 _:k _:2142054851264.
+    _:2142053379456 rdf:_0 _:2142054854336.
+    _:2142053379456 _:id 2142053379456.
+    _:2142054851264 _:id 2142054851264.
+    _:2142054851264 _:k "asdf".
+    _:2142054854336 _:id 2142054854336.
+    _:2142054854336 _:k 3.
+    _:2142054854336 _:kk "asdf".
+    _:2142054860992 _:id 2142054860992.
+    _:2142054860992 _:k _:2142053379456.
+    _:2142054860992 _:kk _:2142013828288.
+    """
+
+
+
+def test():
+    # from rdflib import Graph
+    # a = rdf()
+    # a = Graph().parse(data=a, format='text/turtle')
+
+    _ = json()
+    from json2rdf.json2rdf import to_rdf
+    f = to_rdf(_, subject_keys=('id',) )
+    print(f)
+    #f = Graph().parse(data=f, format='text/turtle')
+
+    #from rdflib.compare import isomorphic
+    #assert(isomorphic(f, a))
+    
