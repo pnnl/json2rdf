b6d82557f4c498f5895806bdfbb7f8023aa06b47
Majid alDosari <majid.aldosari@pnnl.gov>
2024-12-20 17:13:14 -0500

    chk ver task

--- tasks.py
+++ tasks.py
@@ -1,21 +1,23 @@
 
 
-def run(*p, **k):
-    from subprocess import run
-    from pathlib import Path
-    _ = run(*p, cwd=Path(__file__).parent, **k,)
-    if _.stdout: print(_.stdout)
-    if _.stderr: print(_.stderr)
-    return _
-
 def build(commit=False):
+    def run(cmd, *p, **k):
+        from subprocess import check_call as run
+        from pathlib import Path
+        return run(cmd, *p, cwd=Path(__file__).parent, **k)
     if commit:
-        run('uvx hatchling version major')
-        run('uv lock --upgrade-package json2rdf')
-        run('git add -u') # https://github.com/pre-commit/pre-commit/issues/747#issuecomment-386782080
+        run('uvx hatchling version major', )
+        run('uv lock --upgrade-package json2rdf', )
+        # https://github.com/pre-commit/pre-commit/issues/747#issuecomment-386782080
+        run('git add -u', )
     run('uv build')
 
+def chk_ver(rev='master'):
+    from json2rdf import __version__ as v
+    from subprocess import check_output as run
+    c = run(f'git rev-list --count {rev}', text=True).strip()
+    return v == c
 
 if __name__ == '__main__':
     from fire import Fire
-    Fire({'build': build})
+    Fire({f.__name__:f for f in {build, chk_ver}})
