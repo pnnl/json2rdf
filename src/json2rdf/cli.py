2222e2c60ab1740109572b03c84a2138f0d53316
Majid alDosari <majid.aldosari@pnnl.gov>
2025-07-23 18:37:35 -0400

    rem old cli opts

--- src/json2rdf/cli.py
+++ src/json2rdf/cli.py
@@ -3,8 +3,7 @@ from inspect import signature as sig
 from . import json2rdf as j2r
 args = sig(j2r).parameters
 def _(
-        i=Path('data.json'), imeta=None,
-        asserted =          args['asserted']        .default,
+        i=Path('data.json'),
         sort =              args['sort']            .default,
         # id interpretation
         subject_id_keys =   args['subject_id_keys'] .default,
@@ -12,21 +11,17 @@ def _(
         # uri construction
         id_prefix =         args['id_prefix']       .default,
         key_prefix =        args['key_prefix']      .default,
-        meta_prefix =       args['meta_prefix']     .default,
         o: Path|None=None,#Path('data.ttl'),
         ):
     from json import load
     data = load(open(i))
-    meta = load(open(imeta)) if imeta else args['meta'].default
     _ = j2r(
-            data, meta=meta,
-            asserted=asserted,
+            data,
             sort=sort,
             subject_id_keys=subject_id_keys,
             object_id_keys=object_id_keys,
             id_prefix=id_prefix,
-            key_prefix=key_prefix,
-            meta_prefix=meta_prefix,)
+            key_prefix=key_prefix,)
     if not o:
         return _
     else:
